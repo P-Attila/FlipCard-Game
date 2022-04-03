696c60ce24feef2208de7ca9f14ee7905770579a
P-Attila <94472248+P-Attila@users.noreply.github.com>
2022-04-03 19:32:29 +0200

    card fix

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,19 +1,18 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 function Card({ frontSide, backSide }) {
 	const [isFront, changeFace] = useState(true);
 	function handleClick() {
-		changeFace(oldState => !oldState)
+		changeFace((oldState) => !oldState);
 	}
-    const text = isFront ? frontSide : backSide;
-    const sideClass = isFront ? "front" : "back";
-    const classList = `flash-card ${sideClass}`;
+	const text = isFront ? frontSide : backSide;
+	const sideClass = isFront ? "front" : "back";
+	const classList = `flash-card ${sideClass}`;
 	return (
 		<div className={classList} onClick={handleClick}>
 			{text}
 		</div>
-	)
+	);
 }
 
 const Game = () => {
@@ -28,46 +27,43 @@ const Game = () => {
 					<p className="py-4">Click on a card to reveal the secret solution.</p>
 				</div>
 
-				<div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 hover:cursor-pointer">
-					<div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+				<div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-8 text-center py-8 hover:cursor-pointer">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="9 * 6" backSide="54" />
 					</div>
-					<div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="2 ** 8" backSide="256" />
 					</div>
-					<div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="Rabbit * 8" backSide="Rabbyte" />
 					</div>
-					<div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="6 * 5" backSide="30" />
 					</div>
-
-					<div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="2 * 8" backSide="16" />
 					</div>
-					<div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="3 * 4" backSide="12" />{" "}
 					</div>
-          <div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="3 * 8" backSide="24" />
 					</div>
-					<div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="4 * 5" backSide="20" />{" "}
 					</div>
-
-          <div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="2 * 7" backSide="14" />
 					</div>
-					<div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="4 * 7" backSide="28" />{" "}
 					</div>
-          <div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="5 * 5" backSide="25" />
 					</div>
-					<div className="shadow-md shadow-teal-400 rounded-md py-5 hover:scale-110 duration-500">
+					<div className="w-54 h-16 boxshadow rounded-md hover:scale-110 duration-500">
 						<Card frontSide="7 * 8" backSide="56" />{" "}
 					</div>
-
 				</div>
 			</div>
 		</div>
